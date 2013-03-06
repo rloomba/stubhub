ed17d23fbb9e49be5cf3f96417a3e013be429fb6
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-05 22:05:53 -0500

    added search tix by event_id feature

--- lib/stubhub/ticket.rb
+++ lib/stubhub/ticket.rb
@@ -6,7 +6,12 @@ module Stubhub
                  "id" => "#{ticket_id}"}
       Client.make_request(Ticket, options)
     end
-
+    
+    def self.find_by_event_id(event_id)
+      options = { "stubhubDocumentType" => "#{demodulize(self).downcase}",
+                  "event_id" => "#{event_id}"}
+      Client.make_request(Ticket, options)
+    end
 
   end
 end
