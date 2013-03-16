e40490a5dde4dad928dcc6a4bfb90f7d06f889c4
Ryan Loomba <ryan.loomba@gmail.com>
2013-03-16 11:05:49 -0700

    added document specs

---
+++ spec/document_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+module Stubhub
+  describe Document do
+    context "#initialize" do
+      it 'raises an error if passed wrong data' do
+        expect {
+          Document.new(1)}.to raise_error(ArgumentError) 
+      end
+    end
+
+    context "#fields" do
+      it 'returns an array of keys for a hash' do
+        Document.new({a:1, b:2}).fields.should eq(["a","b"])
+      end
+    end
+
+    context ".demodulize" do
+      it "returns only the class name without a namespace" do
+        Document.demodulize(Stubhub::Document).should eq "Document"
+      end
+    end
+
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'stubhub'
+require 'vcr'
+
+
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  
+  config.order = 'random'
+end
