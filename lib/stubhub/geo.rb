41a9697d56ee758f7f7c67c3b4324c5e49f7e015
John Kelly <jwkellyiii@gmail.com>
2013-04-01 19:38:07 -0400

    Implemented the GEO location API.

--- lib/stubhub/geo.rb
+++ lib/stubhub/geo.rb
@@ -1,3 +1,29 @@
 module Stubhub
-  class Geo < Document;end
+  class Geo < Document
+      def self.find_by_geo_id(geo_id, options = {})
+      params = {"stubhubDocumentType" => "#{demodulize(self).downcase}",
+                "geoId" => "#{geo_id}"}
+      Client.make_request(Geo, params, options)
+    end
+
+    # rough matching. i.e. "San Francisco" will match "San Diego" due to the "+San+"
+    def self.find_by_locale(locale, options = {})
+      params = {"stubhubDocumentType" => "#{demodulize(self).downcase}",
+                "localeDescription" => "#{URI::escape(locale)}"}
+      Client.make_request(Geo, params, options)
+    end
+
+    def self.search(search_query, options = {})
+      search_query = URI.escape(search_query)
+      params = {"stubhubDocumentType" => "#{demodulize(self).downcase}",
+                "description" => "#{search_query}"}
+      Client.make_request(Geo, params, options)
+    end
+
+    def events(options = {})
+      params = {"stubhubDocumentType" => "event",
+                "ancestorGeoIds" => "#{self.geoId}"}
+      Client.make_request(Event, params, options)
+    end
+  end
 end
--- spec/geo_spec.rb
+++ spec/geo_spec.rb
@@ -1,1 +1,37 @@
 require 'spec_helper'
+GEO_ID = 81989
+LOCALE = "SF Bay Area"
+
+module Stubhub
+  describe Geo do
+    before :each do
+      VCR.insert_cassette 'geo' do
+      end
+    end
+    after :each do
+      VCR.eject_cassette
+    end
+
+    context ".find_by_geo_id" do
+      it "finds the geo with geoId #{GEO_ID}" do
+        geo = Geo.find_by_geo_id(GEO_ID)
+        geo.id.should eq(GEO_ID.to_s)
+      end
+    end
+
+    context ".find_by_locale" do
+      it "finds the geos within the locale #{LOCALE}" do
+        geos = Geo.find_by_locale(LOCALE)
+        geos.length.should be >= 1
+      end
+    end
+
+    context ".search" do
+      it "peforms a geo search for the keyword" do
+        geos = Geo.search("Bob Hope")
+        geos.length.should be >= 1
+      end
+    end
+
+  end
+end
