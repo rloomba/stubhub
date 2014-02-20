7e4f297b191075fa209452a38fc7e641fdc55105
Ryan Urabe <rurabe@gmail.com>
2014-02-20 00:23:55 -0500

    made proxy get

--- lib/stubhub/proxy_client.rb
+++ lib/stubhub/proxy_client.rb
@@ -2,7 +2,10 @@ module Stubhub
   class ProxyClient < Client
     class << self
       def get(url)
-        p url
+        uri = URI(url)
+        Net::HTTP.new(uri.host,uri.port,ENV['PROXY_ADDRESS'],ENV['PROXY_PORT']).start do |http|
+          http.get(uri)
+        end.body
       end
 
     end
